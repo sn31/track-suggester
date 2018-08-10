93311b643c8514f233c2d8870e31917b6c976800
SkyeN <skye@dames.es>
2018-08-10 11:13:23 -0700

    add fields for user information, bootstrap styling and seperate back and front end logics

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,41 @@ b - Java
 c - PHP
 */
 
+//BACK-END LOGIC
+var findResult = function (answer1, answer2, answer3, answer4, answer5) {
+    var answerArray = [answer1, answer2, answer3, answer4, answer5];
+    var i = 0;
+    var aCount = 0;
+    var bCount = 0;
+    var cCount = 0;
+
+    for (i=0;i<answerArray.length;i++) {
+        if (answerArray[i] === "a") {
+            aCount++;
+        }
+        else if (answerArray[i] === "b") {
+            bCount++;
+        }
+        else {
+            cCount++;
+        }
+    }
+    var resultCount = Math.max(aCount, bCount, cCount);
+
+    var result ="";
+    if (resultCount === aCount) {
+        result = "Python";
+    }
+    else if (resultCount === bCount) {
+        result = "Java";
+    }
+    else {
+        result = "PHP";
+    }
+    return result;
+}
+
+// FRONT-END LOGIC
 $(document).ready(function () {
     $("form#surveyInput").submit(function (event) {
         event.preventDefault();
@@ -14,38 +49,9 @@ $(document).ready(function () {
         var answer4 = $("input:radio[name=q4]:checked").val();
         var answer5 = $("input:radio[name=q5]:checked").val();
        
-        var answerArray = [answer1, answer2, answer3, answer4, answer5];
-
-        var i = 0;
-        var aCount = 0;
-        var bCount = 0;
-        var cCount = 0;
-
-        for (i=0;i<answerArray.length;i++) {
-            if (answerArray[i] === "a") {
-                aCount++;
-            }
-            else if (answerArray[i] === "b") {
-                bCount++;
-            }
-            else {
-                cCount++;
-            }
-        }
-        var resultCount = Math.max(aCount, bCount, cCount);
-
-        var result ="";
-        if (resultCount === aCount) {
-            result = "Python";
-        }
-        else if (resultCount === bCount) {
-            result = "Java";
-        }
-        else {
-            result = "PHP";
-        }
-
-        $(".result").text(result);
+        var test = findResult(answer1, answer2, answer3, answer4, answer5);
+        console.log(test);
+        $(".result").text(findResult(answer1, answer2, answer3, answer4, answer5));
         $("#result").show();
     });
 });
